4f1f3c54e387dfaaec5ebcd51a93154f9e8c0f68
Ian Williamson <ianwill2014@gmail.com>
2025-12-12 09:49:59 -0600

    added more missing variables

---
+++ infra/modules/resource_group/variables.tf
@@ -0,0 +1,9 @@
+variable "name_prefix" {
+  type        = string
+  description = "Prefix used for naming resources"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region for the identity"
+}
