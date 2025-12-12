bb1cfbb75ac1c373ff1e1bbde1a28237df66dc04
Ian Williamson <ianwill2014@gmail.com>
2025-12-12 09:42:30 -0600

    cleaned up providers

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,19 +1,5 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-    }
-  }
-
-  backend "local" {}
-}
-
 provider "azurerm" {
   features {}
-
-  use_oidc     = true
-  client_id    = var.oidc_client_id
-  tenant_id    = var.tenant_id
   subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
 }
--- infra/versions.tf
+++ infra/versions.tf
@@ -4,13 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = "~> 3.100.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.0"
     }
   }
-
-  backend "azurerm" {}
-}
-
-provider "azurerm" {
-  features {}
 }
