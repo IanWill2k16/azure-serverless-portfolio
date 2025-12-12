331b6198276e30b41529e3fbf85b59dbc2b2116e
Ian Williamson <ianwill2014@gmail.com>
2025-12-12 11:48:29 -0600

    removed duplicate variable

--- infra/modules/identity/main.tf
+++ infra/modules/identity/main.tf
@@ -3,7 +3,3 @@ resource "azurerm_user_assigned_identity" "id" {
   location            = var.location
   resource_group_name = var.resource_group_name
 }
-
-output "principal_id" {
-  value = azurerm_user_assigned_identity.id.principal_id
-}
