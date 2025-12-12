2fb118d3523464dc4a9a233e5b0f3bef93a399fb
Ian Williamson <ianwill2014@gmail.com>
2025-12-12 15:35:54 -0600

    updated versions to more recent

--- infra/versions.tf
+++ infra/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.7.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.100.0"
+      version = "~> 4.3.0"
     }
   }
 }
