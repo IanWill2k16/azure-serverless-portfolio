051c5866efb33693c71ac4588c9cd8b110f700a3
Ian Williamson <ianwill2014@gmail.com>
2025-12-13 08:49:58 -0600

    fixed syntax error...

--- app/api/function_app.py
+++ app/api/function_app.py
@@ -4,10 +4,10 @@ import json
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.function_name(name="hello")
-@app.route(route="hello")
+@app.route(route="hello", methods=["GET"])
 def hello(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse(
         json.dumps({"message": "Hello from Azure Function API!"}),
-        content_type="application/json"
+        content_type="application/json",
         status_code=200
     )
