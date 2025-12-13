3cbe61bb26eb4c121b99fd30c7a6119ed5727fdf
Ian Williamson <ianwill2014@gmail.com>
2025-12-13 07:01:03 -0600

    fix incorrect variables

--- infra/modules/functionapp/main.tf
+++ infra/modules/functionapp/main.tf
@@ -9,8 +9,8 @@ resource "azurerm_service_plan" "this" {
 
 resource "azurerm_application_insights" "ai" {
   name                = "cloudportfolio-prod-ai"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
   application_type    = "web"
 }
 
