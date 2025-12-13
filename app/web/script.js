8db3213eede3d0cb2b63a5f345a6f7ff9a453e1c
Ian Williamson <ianwill2014@gmail.com>
2025-12-13 13:39:03 -0600

    add variable function url injection

--- app/web/script.js
+++ app/web/script.js
@@ -1,8 +1,11 @@
+
+
+const API_BASE_URL = "__API_BASE_URL__"
 async function loadMessage() {
     const responseElement = document.getElementById("response");
 
     try {
-        const res = await fetch("https://cloudportfolio-prod-fn.azurewebsites.net/api/hello");
+        const res = await fetch(`${API_BASE_URL}/api/hello`);
         const data = await res.json();
         responseElement.textContent = data.message;
     } catch (err) {
