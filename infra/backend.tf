cf925b53652cba4ec98102e14fcc06bad55aa00e
Ian Williamson <ianwill2014@gmail.com>
2025-12-13 15:56:47 -0600

    moved tfstate variable to workflow

--- infra/backend.tf
+++ infra/backend.tf
@@ -3,6 +3,5 @@ terraform {
     resource_group_name  = "cloudportfolio-prod-tfstate-rg"
     storage_account_name = "cloudportfolioprodtf"
     container_name       = "tfstate"
-    key                  = "terraform-${var.environmet}.tfstate"
   }
 }
